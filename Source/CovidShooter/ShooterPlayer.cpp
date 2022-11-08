119b902561f38f9cd5b9cdbc14a8a4423535eba6
KronosGR <geo.elgeo@gmail.com>
2022-11-08 19:55:43 +0100

    spawn the projectile

--- Source/CovidShooter/ShooterPlayer.cpp
+++ Source/CovidShooter/ShooterPlayer.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1da3092945ded848038f39c680162d0191ab009ed4da86ca95dc694acbda7ae8
-size 4745
+oid sha256:7895e8df625b197e53e18089c792075880444ca969cee4eb7c3c40d47925a463
+size 5192
--- Source/CovidShooter/ShooterPlayer.h
+++ Source/CovidShooter/ShooterPlayer.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a3d67197f60cf18da0fdafe40d4f87efacd2d3d15a56e4f85c80d1d35c283f3
-size 2666
+oid sha256:99d206ef6b3b5930173d4cc067a2a1e90427a19c86e634522f634a12ca64b2a1
+size 2873
