2560307b8c77bb8b0c9008bb54ff6c88b3b52878
KronosGR <geo.elgeo@gmail.com>
2022-11-08 20:34:53 +0100

    enemy BP and class  setup

--- Source/CovidShooter/ShooterEnemy.cpp
+++ Source/CovidShooter/ShooterEnemy.cpp
@@ -1,27 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "ShooterEnemy.h"
-
-// Sets default values
-AShooterEnemy::AShooterEnemy()
-{
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-
-}
-
-// Called when the game starts or when spawned
-void AShooterEnemy::BeginPlay()
-{
-	Super::BeginPlay();
-	
-}
-
-// Called every frame
-void AShooterEnemy::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
-
+version https://git-lfs.github.com/spec/v1
+oid sha256:c5cdf2e34f841dace2a16ffc1da5ab0bfe5a9b613ffd19efb98def4cf528c26e
+size 1006
--- Source/CovidShooter/ShooterEnemy.h
+++ Source/CovidShooter/ShooterEnemy.h
@@ -1,26 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#pragma once
-
-#include "CoreMinimal.h"
-#include "GameFramework/Actor.h"
-#include "ShooterEnemy.generated.h"
-
-UCLASS()
-class COVIDSHOOTER_API AShooterEnemy : public AActor
-{
-	GENERATED_BODY()
-	
-public:	
-	// Sets default values for this actor's properties
-	AShooterEnemy();
-
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
-};
+version https://git-lfs.github.com/spec/v1
+oid sha256:0b5f55cab69795697282b5750ede448ea2b9cfb023d22793dad92aebd0e369b0
+size 808
