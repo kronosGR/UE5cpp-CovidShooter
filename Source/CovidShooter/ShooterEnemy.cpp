2eb18f61febcba692797b8d1574039dc781a9ae2
KronosGR <geo.elgeo@gmail.com>
2022-11-09 06:39:32 +0100

    added enemy growth

--- Source/CovidShooter/ShooterEnemy.cpp
+++ Source/CovidShooter/ShooterEnemy.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d48cf49bf18ff3db4c0495406a3975913f6aff4a0f4a981a35fda9fc1f81e7c7
-size 2081
+oid sha256:e8717f2d6e02891c02ba561a033c807fcdb92cb31239ea974b32fb127b9e658f
+size 2425
--- Source/CovidShooter/ShooterEnemy.h
+++ Source/CovidShooter/ShooterEnemy.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:81e028511d203338b19fcb3012fa55fcfc4b13f85963aea3aaf7ad936e78cb0b
-size 1097
+oid sha256:3890f5ff00e852fdbb7d9ab07929226894dba7c46c50847b162f447e7dec71d4
+size 1234
